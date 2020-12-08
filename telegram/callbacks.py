0c540fbf6c96f0549c6fe955ceaed6240d08b494
widegenesis <widegenesis@gmail.com>
2020-12-08 16:01:45 +0200

    autoupdate 2020-12-08-16:01:45

--- telegram/callbacks.py
+++ telegram/callbacks.py
@@ -185,11 +185,11 @@ async def callback_handler(event, client, img_path=None, yahoo_path=None, engine
         with open(filename5, newline='') as f5:
             data5 = csv.reader(f5, delimiter=',')
             for row5 in data5:
-                row5 = str(row5).strip("[']ValueDate")
+                row5 = str(row5).strip("[']")
                 if row5 != 'Value' or 'Date':
                     await client.send_message(entity=entity, message=f'{row5}')
                 else:
-                    pass
+                    continue
         await client.edit_message(message, 'Кривая доходности и дивиденды')
         await event.edit()
         await client.send_message(event.input_sender, 'Как интерпритировать кривую доходности? /instruction02',
