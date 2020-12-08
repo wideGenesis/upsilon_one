60b8a3028f20b95083c1afba04914b649d2c3ba2
widegenesis <widegenesis@gmail.com>
2020-12-08 15:51:41 +0200

    autoupdate 2020-12-08-15:51:41

--- telegram/callbacks.py
+++ telegram/callbacks.py
@@ -184,10 +184,7 @@ async def callback_handler(event, client, img_path=None, yahoo_path=None, engine
         filename5 = os.path.join(img_path, 'spx_yield.csv')
         with open(filename5, newline='') as f5:
             data5 = csv.reader(f5, delimiter=',')
-            for row5 in data5:
-                if row5 < 1:
-                    continue
-            else:
+            for row5 in data5[1:2]:
                 row5 = str(row5).strip("[']")
                 await client.send_message(entity=entity, message=f'{row5}')
         await client.edit_message(message, 'Кривая доходности и дивиденды')
