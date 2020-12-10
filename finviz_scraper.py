4f988d0cb01b827ea9eebedb636b101b9e422623
widegenesis <widegenesis@gmail.com>
2020-12-10 23:12:48 +0200

    autoupdate 2020-12-10-23:12:48

--- finviz_scraper.py
+++ finviz_scraper.py
@@ -1,36 +1,14 @@
 import finviz
-from finviz.screener import Screener
 import os
 import quantstats as qs
 
 stock = 'QQQ'
-# stock_list = ['QQQ']
-#
-# stock_ = Screener(tickers=stock_list, table='Performance', order='price')
-#
-# price = finviz.get_stock(stock)
-# news = finviz.get_news(stock)
-#
-# print(price)
-# print(news)
-#
-#
-# chart = stock_.get_charts(period='d', chart_type='c', size='m', ta='0')
-# print(chart)
 
-# period='d' > daily
-# period='w' > weekly
-# period='m' > monthly
+price = finviz.get_stock(stock)
+news = finviz.get_news(stock)
 
-# chart_type='c' > candle
-# chart_type='l' > lines
-
-# size='m' > small
-# size='l' > large
-
-# ta='1' > display technical analysis
-# ta='0' > ignore technical analysis
-
+print(price)
+print(news)
 
 # extend pandas functionality with metrics, etc.
 qs.extend_pandas()
