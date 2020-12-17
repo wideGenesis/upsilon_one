5e35eda279b3b9c121be731e3cb269ad51aaa52e
widegenesis <widegenesis@gmail.com>
2020-12-17 21:54:43 +0200

    autoupdate 2020-12-17-21:54:43

--- quotes/stock_quotes_news.py
+++ quotes/stock_quotes_news.py
@@ -1,40 +1,115 @@
-import os
+from dataclasses import dataclass
+import pandas as pd
+import json
 import finviz
 import quantstats as qs
 
 
-def stock_description(stock=None, limit_=None):
-    try:
-        price = finviz.get_stock(stock)
-    except ValueError as e10:
-        return 'Ticker not found'
-    msg1 = price['Company'] + '\n' + \
-        'Sector: ' + price['Sector'] + '\n' + \
-        'Industry: ' + price['Industry'] + '\n' + \
-        'Country: ' + price['Country'] + '\n' + \
-        'MarketCap: ' + price['Market Cap'] + '\n' + \
-        'Price: ' + price['Price'] + '\n' + \
-        'SMA50: ' + price['SMA50'] + '\n' + \
-        'AvgVolume: ' + price['Avg Volume'] + '\n'
-    return msg1
+@dataclass
+class StockStat:
+    __slots__ = [
+        'stock',
+        'returns',
+        'limit_',
+        'title',
+        'ticker',
+        'benchmark',
+        'mode',
+        'ticker_',
+        'display',
+    ]
 
+    def __init__(self,
+                 stock: str = None,
+                 returns: pd = None,
+                 limit_: int = None,
+                 title: str = None,
+                 benchmark: str = 'QQQ',
+                 mode: str = 'full',
+                 ticker_: str = None,
+                 display: bool = False,
+                 ):
+        self.stock = stock
+        self.returns = returns
+        self.limit_ = limit_
+        self.title = title
+        self.benchmark = benchmark
+        self.mode = mode
+        self.ticker_ = ticker_
+        self.display = display
 
-def stock_quotes(stock=None, limit_=None):
-    img = f'results/ticker_stat/{stock}.png'
-    try:
-        returns = qs.utils.download_returns(stock)
-    except ValueError as e11:
-        return 'Ticker not found'
-    qs.plots.snapshot_v2(returns, title=f'{stock}', savefig=img, show=False, ticker=stock)
-    msg2 = qs.reports.metrics_v2(returns, benchmark='QQQ', mode='full', ticker_=stock,  display=False)
+    def stock_download(self):
+        try:
+            returns = qs.utils.download_returns(self.stock)
+        except ValueError as e11:
+            print('Ticker not found')
+            return
+        self.returns = returns
 
+    def stock_snapshot(self):
+        img = f'results/ticker_stat/{self.stock}.png'
+        qs.plots.snapshot_v2(self.returns,
+                             title=f'{self.stock}',
+                             savefig=img)
+    def stock_stat(self):
+        stats = qs.reports.metrics_v2(self.returns,
+                                      benchmark=self.benchmark,
+                                      mode=self.mode,
+                                      ticker_=self.stock,
+                                      display=self.display)
+        parse = json.loads(stats)
+        msg = 'Ключевые Характеристики: ' '\n' + 'Start Period ' + parse[self.stock]['Start Period'] + '\n' + \
+              'End Period ' + parse[self.stock]['End Period'] + '\n' + \
+              "Total Return % " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Total Return '])\
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Total Return ']) + '\n' + \
+              "CAGR% " + '\n' + f'{self.stock} ' + str(parse[self.stock]['CAGR%'])\
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['CAGR%']) + '\n' + \
+              "Sharpe " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Sharpe']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Sharpe']) + '\n' + \
+              "Volatility (ann.) % " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Volatility (ann.) ']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Volatility (ann.) ']) + '\n' + \
+              "Expected Monthly % " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Expected Monthly %']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Expected Monthly %']) + '\n' + \
+              "Expected Yearly % " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Expected Yearly %']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Expected Yearly %']) + '\n' + \
+              "Kelly Criterion % " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Kelly Criterion ']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Kelly Criterion ']) + '\n' + \
+              "Daily Value-at-Risk " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Daily Value-at-Risk ']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Daily Value-at-Risk ']) + '\n' + \
+              "YTD " + '\n' + f'{self.stock} ' + str(parse[self.stock]['YTD ']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['YTD ']) + '\n' + \
+              "Best Year " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Best Year ']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Best Year ']) + '\n' + \
+              "Worst Year " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Worst Year ']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Worst Year ']) + '\n' + \
+              "Alpha " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Alpha']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Alpha']) + '\n' + \
+              "Beta " + '\n' + f'{self.stock} ' + str(parse[self.stock]['Beta']) \
+              + f'| {self.benchmark} ' + str(parse['Benchmark']['Beta'])
 
-def stock_news(stock=None, limit_=None):
-    try:
-        news = finviz.get_news(stock)
-    except ValueError as e10:
-        return 'Ticker not found'
-    msg = ''
-    for n in news[0:15]:
-        msg += ' '.join(n) + '\n' + '\n'
-    return msg
+        return msg
+
+    def stock_description(self):
+        try:
+            price = finviz.get_stock(self.stock)
+        except ValueError as e10:
+            return 'Ticker not found'
+        msg1 = price['Company'] + '\n' + \
+            'Sector: ' + price['Sector'] + '\n' + \
+            'Industry: ' + price['Industry'] + '\n' + \
+            'Country: ' + price['Country'] + '\n' + \
+            'MarketCap: ' + price['Market Cap'] + '\n' + \
+            'Price: ' + price['Price'] + '\n' + \
+            'SMA50: ' + price['SMA50'] + '\n' + \
+            'AvgVolume: ' + price['Avg Volume'] + '\n'
+        return msg1
+
+    def stock_news(self):
+        try:
+            news = finviz.get_news(self.stock)
+        except ValueError as e10:
+            return 'Ticker not found'
+        msg = ''
+        for n in news[0:15]:
+            msg += ' '.join(n) + '\n' + '\n'
+        return msg
