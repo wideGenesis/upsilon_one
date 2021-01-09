9348c7122c3b8a4b3cecc37c146b724a3ad63cc0
defoer <defoer@gmail.com>
2021-01-09 20:58:04 +0300

    вторая часть шедулера

--- cron_calc_portfolios_allocation.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-BASEDIR=/home/upsilonsfather/projects/ups_one
-
-source $BASEDIR/venv/bin/activate
-#echo "[$(date +'%Y-%m-%d %H:%M:%S')]************* Start  cron_calc_portfolios_allocation *************" > $BASEDIR/logs/stdout.log
-python3 $BASEDIR/cron_calc_portfolios_allocation.py &
-deactivate
---
+++ cron_scheduler.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+# For tests run every 1 minutes
+# * * * * * /home/upsilonsfather/projects/ups_one/cron_scheduler.sh "1"
+#
+# For tests run every 5 minutes
+# */5 * * * * /home/upsilonsfather/projects/ups_one/cron_scheduler.sh "5"
+#
+# Start every 30 min
+# */30 * * * * /home/upsilonsfather/projects/ups_one/cron_scheduler.sh "30"
+#
+# Start every 120 min  (в первую минуту, каждого 2го часа)
+# 1 */2 * * * /home/upsilonsfather/projects/ups_one/cron_scheduler.sh "120"
+#
+# Start every 480 min  (в пятую минуту, каждого 8го часа)
+# 5 */8 * * * /home/upsilonsfather/projects/ups_one/cron_scheduler.sh "480"
+#
+# Start every 720 min  (в десятую минуту, каждого 12го часа)
+# 10 */12 * * * /home/upsilonsfather/projects/ups_one/cron_scheduler.sh "720"
+#
+# Start everyday  (каждый день в 3:00)
+# 0 3 * * * /home/upsilonsfather/projects/ups_one/cron_scheduler.sh "EVERYDAY"
+#
+# Start monday  (каждый понедельник в 3:10)
+# 10 3 * * 0 /home/upsilonsfather/projects/ups_one/cron_scheduler.sh "MONDAY"
+#
+# Start first date of month  (каждое первое число месяца в 4:00)
+# 0 4 1 * * /home/upsilonsfather/projects/ups_one/cron_scheduler.sh "FIRST_DAY_OF_MONTH"
+#
+#
+#
+BASEDIR=/home/upsilonsfather/projects/ups_one
+LOGDIR=$BASEDIR/logs
+
+source $BASEDIR/venv/bin/activate
+echo "[$(date +'%Y-%m-%d %H:%M:%S')]************* Start  cron scheduler *************" > $LOGDIR/cron_scheduler.log
+if [ "$1" == "1" ]
+then
+  echo "#Every  $1 [$(date +'%Y-%m-%d %H:%M:%S')]"  > $LOGDIR/cron_scheduler.log
+elif [ "$1" == '5' ]
+then
+  echo "#Every $1 [$(date +'%Y-%m-%d %H:%M:%S')]"  > $LOGDIR/cron_scheduler.log
+elif [ "$1" == '30' ]
+then
+  echo "#Every $1 [$(date +'%Y-%m-%d %H:%M:%S')]"  > $LOGDIR/cron_scheduler.log
+  python3 $BASEDIR/cron_every_30_min.py  > $LOGDIR/cron_scheduler.log
+elif [ "$1" == '120' ]
+then
+  echo "#Every $1 [$(date +'%Y-%m-%d %H:%M:%S')]"  > $LOGDIR/cron_scheduler.log
+  python3 $BASEDIR/cron_every_120_min.py  > $LOGDIR/cron_scheduler.log
+elif [ "$1" == '480' ]
+then
+  echo "#Every $1 [$(date +'%Y-%m-%d %H:%M:%S')]"  > $LOGDIR/cron_scheduler.log
+  python3 $BASEDIR/cron_every_480_min.py  > $LOGDIR/cron_scheduler.log
+elif [ "$1" == '720' ]
+then
+  echo "#Every $1 [$(date +'%Y-%m-%d %H:%M:%S')]"  > $LOGDIR/cron_scheduler.log
+  python3 $BASEDIR/cron_every_720_min.py  > $LOGDIR/cron_scheduler.log
+elif [ "$1" == 'MONDAY' ]
+then
+  echo "#Every $1 [$(date +'%Y-%m-%d %H:%M:%S')]"  > $LOGDIR/cron_scheduler.log
+  python3 $BASEDIR/cron_every_monday.py  > $LOGDIR/cron_scheduler.log
+elif [ "$1" == 'EVERYDAY' ]
+then
+  echo "#Every $1 [$(date +'%Y-%m-%d %H:%M:%S')]"  > $LOGDIR/cron_scheduler.log
+  python3 $BASEDIR/cron_update_closes_capital_returns.py  > $LOGDIR/cron_scheduler.log
+elif [ "$1" == 'FIRST_DAY_OF_MONTH' ]
+then
+  echo "#Every $1 [$(date +'%Y-%m-%d %H:%M:%S')]"  > $LOGDIR/cron_scheduler.log
+  python3 $BASEDIR/cron_get_universe_holdings.py  > $LOGDIR/cron_scheduler.log
+  python3 $BASEDIR/cron_calc_portfolios_allocation.py  > $LOGDIR/cron_scheduler.log
+fi
+deactivate
