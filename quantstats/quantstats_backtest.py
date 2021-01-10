5fd73da1f687302cb255897e26e00ab17cc7a96b
widegenesis <widegenesis@gmail.com>
2021-01-10 21:45:20 +0200

    autoupdate 2021-01-10-21:45:20

--- quantstats/quantstats_backtest.py
+++ quantstats/quantstats_backtest.py
@@ -5,19 +5,11 @@ import quantstats as qs
 import pdfkit
 
 
-def quantstats_pdf(port_rets_df=None, bench='QQQ', filename=None, title=None):
-    df = qs.utils.download_returns(bench, period='10y')
-    print(df)
-    print(df.index)
-    print(port_rets_df)
-    print(port_rets_df.index)
-
-
-
+def quantstats_pdf(port_rets_df=None, bench=None, filename=None, title=None):
+    df = qs.utils.download_returns(bench, period='max')
     # mom1 = ((df1[col].iloc[-1] - df1[col].iloc[0]) / df1[col].iloc[0])
-    bench_df = qs.utils.download_returns(bench, period='10y')
 
-    qs.reports.html(returns=df, output=f'{filename}.html', title=title)
+    qs.reports.html(returns=port_rets_df, benchmark=df, output=f'{filename}.html', title=title)
     pdfkit_options = {
                 'margin-top': '0.1',
                 'margin-right': '0.1',
@@ -34,12 +26,14 @@ def quantstats_pdf(port_rets_df=None, bench='QQQ', filename=None, title=None):
     pdfkit.from_file(f'{filename}.html', f'{filename}.pdf', options=pdfkit_options)
     print('Stat pdf has been converted')
 
-x = get_portfolio_returns_df('parking', start_date=None, end_date=date.today())
+parking = get_portfolio_returns_df('parking', start_date=None, end_date=date.today())
+allweather = get_portfolio_returns_df('allweather', start_date=None, end_date=date.today())
+balanced = get_portfolio_returns_df('balanced', start_date=None, end_date=date.today())
+aggressive = get_portfolio_returns_df('aggressive', start_date=None, end_date=date.today())
+leveraged = get_portfolio_returns_df('leveraged', start_date=None, end_date=date.today())
 
-quantstats_pdf(port_rets_df=x, bench='QQQ', filename='parking', title='parking')
-# print(x)
-
-# Date,Aggressive
-# 1/31/08,-0.0636
-# 2/29/08,-0.0091
-# 3/31/08,0.0092
+quantstats_pdf(port_rets_df=parking, bench='TLT', filename='parking', title='Parking Strategy')
+quantstats_pdf(port_rets_df=allweather, bench='SPY', filename='allweather', title='All Weather Strategy')
+quantstats_pdf(port_rets_df=balanced, bench='QQQ', filename='balanced', title='Balanced Strategy')
+quantstats_pdf(port_rets_df=aggressive, bench='QQQ', filename='aggressive', title='Aggressive Strategy')
+quantstats_pdf(port_rets_df=leveraged, bench='QQQ', filename='leveraged', title='Leveraged Strategy')
