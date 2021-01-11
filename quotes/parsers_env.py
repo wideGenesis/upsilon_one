c303c9a2984d7d492913634fe13b9d9c8d1339b8
defoer <defoer@gmail.com>
2021-01-12 01:23:54 +0300

    Update parsers_env.py

--- quotes/parsers_env.py
+++ quotes/parsers_env.py
@@ -20,7 +20,7 @@ def agents():
     #
     # ]
     # return choice(ua)
-    ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15'
+    ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:78.0) Gecko/20100101 Firefox/78.0'
     return ua
 
 
