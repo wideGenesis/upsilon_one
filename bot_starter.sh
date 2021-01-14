55393e0176e09624925d4e7224cf02775cce3beb
defoer <defoer@gmail.com>
2021-01-14 16:47:56 +0300

    Update bot_starter.sh

--- bot_starter.sh
+++ bot_starter.sh
@@ -7,7 +7,7 @@ source $BASEDIR/venv/bin/activate
 echo "[$(date +'%Y-%m-%d %H:%M:%S')]************* Start bot *************" > $LOGDIR/bot.log
 cd $BASEDIR
 # shellcheck disable=SC2069
-python3 $BASEDIR/upsilon_bot.py 2>&1 > $LOGDIR/bot.log &
+python3 $BASEDIR/upsilon_bot.py 2>&1 >> $LOGDIR/bot.log &
 deactivate
 if [[ -f $BASEDIR/do_not_restart.txt ]]
 then
