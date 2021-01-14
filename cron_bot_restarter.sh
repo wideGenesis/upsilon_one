f810efc20388e82f60fd722858ed4a35fa13a776
defoer <defoer@gmail.com>
2021-01-14 18:00:35 +0300

    Правильно работаем с логами

--- cron_bot_restarter.sh
+++ cron_bot_restarter.sh
@@ -5,15 +5,20 @@ LOGDIR=$BASEDIR/logs
 
 amount=$(ps -ef | grep "python3" | grep "upsilon_bot.py" | wc -l)
 
-#whoami >> $BASEDIR/cron_strter.log
+if [[ ! -f $LOGDIR/cron_bot_restarter.log ]]
+then
+   echo "Create log + $(date +'%Y-%m-%d %H:%M:%S')" > $LOGDIR/cron_bot_restarter.log
+fi
+
+echo "Start cron restarter + $(date +'%Y-%m-%d %H:%M:%S')" >> $LOGDIR/cron_bot_restarter.log
 
 if [[ $amount == 0 ]] && [[ ! -f $BASEDIR/do_not_restart.txt ]]
 then
-   #echo "Try start bot + $(date +'%Y-%m-%d %H:%M:%S')" >> $BASEDIR/cron_strter.log
+   echo "Try restart bot + $(date +'%Y-%m-%d %H:%M:%S')" >> $LOGDIR/cron_bot_restarter.log
    cd $BASEDIR
    source $BASEDIR/venv/bin/activate
-   python3 $BASEDIR/upsilon_bot.py  > $LOGDIR/bot.log 2>&1 &
+   python3 $BASEDIR/upsilon_bot.py  >> $LOGDIR/bot.log 2>&1 &
    deactivate
 fi
-#echo "END cron restarter + $(date +'%Y-%m-%d %H:%M:%S')" >> $BASEDIR/cron_strter.log
+echo "END cron restarter + $(date +'%Y-%m-%d %H:%M:%S')" >> $LOGDIR/cron_bot_restarter.log
 
