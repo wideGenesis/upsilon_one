5b314c7c93adda5e32e1f0dbaae24bb21aac36c1
defoer <defoer@gmail.com>
2021-01-15 14:11:31 +0300

    Убрал лишнее логирование

--- cron_bot_restarter.sh
+++ cron_bot_restarter.sh
@@ -10,7 +10,7 @@ then
    echo "Create log + $(date +'%Y-%m-%d %H:%M:%S')" > $LOGDIR/cron_bot_restarter.log
 fi
 
-echo "Start cron restarter + $(date +'%Y-%m-%d %H:%M:%S')" >> $LOGDIR/cron_bot_restarter.log
+#echo "Start cron restarter + $(date +'%Y-%m-%d %H:%M:%S')" >> $LOGDIR/cron_bot_restarter.log
 
 if [[ $amount == 0 ]] && [[ ! -f $BASEDIR/do_not_restart.txt ]]
 then
@@ -20,5 +20,5 @@ then
    python3 $BASEDIR/upsilon_bot.py  >> $LOGDIR/bot.log 2>&1 &
    deactivate
 fi
-echo "END cron restarter + $(date +'%Y-%m-%d %H:%M:%S')" >> $LOGDIR/cron_bot_restarter.log
+#echo "END cron restarter + $(date +'%Y-%m-%d %H:%M:%S')" >> $LOGDIR/cron_bot_restarter.log
 
