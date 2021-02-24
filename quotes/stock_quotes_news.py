36267dd63fd267a21281ace3c2df37b8fae8dc94
widegenesis <widegenesis@gmail.com>
2021-02-24 21:31:37 +0200

    autoupdate 2021-02-24-21:31:37

--- quotes/stock_quotes_news.py
+++ quotes/stock_quotes_news.py
@@ -4,6 +4,7 @@ import json
 import finviz
 from finvizfinance.news import News
 import quantstats as qs
+from quotes.historical_universe import *
 
 
 @dataclass
@@ -102,6 +103,94 @@ class StockStat:
             'SMA50: ' + price['SMA50'] + '\n' + \
             'AvgVolume: ' + price['Avg Volume'] + '\n'
         return msg1
+
+    def company_rank(self):
+        try:
+            rank = get_company_rank(self.stock)
+            price = finviz.get_stock(self.stock)
+            sma50 = price['SMA50'].split('%')
+            sma50 = float(sma50[0])
+            print(rank[0], rank[1], rank[2])
+            print(sma50)
+
+        except Exception as e11:
+            msg1 = 'Ранг для ETF недоступен'
+            return msg1
+
+        if rank[0] is True and rank[1] is True and rank[2] is True and sma50 > 0:
+            msg1 = 'Развивающаяся, прибыльная компания с перспективами роста и моментумом. ' + \
+                   '\U0001F44D'*5 + ' - Сильный кандидат для покупки'
+            return msg1
+        elif rank[0] is True and rank[1] is True and rank[2] is True and sma50 < 0:
+            msg1 = 'Развивающаяся, прибыльная компания с перспективами роста. ' + \
+                   '\U0001F44D'*4 + ' - Хороший кандидат для покупки'
+            return msg1
+        elif rank[0] is True and rank[1] is True and rank[2] is False and sma50 > 0:
+            msg1 = 'Стабильная, прибыльная компания с перспективами роста или компания ' \
+                   'оптимизирующая рентабельность бизнеса без расширения рынков сбыта. ' + \
+                   '\U0001F44D'*4 + ' - Хороший кандидат для покупки'
+            return msg1
+        elif rank[0] is True and rank[1] is True and rank[2] is False and sma50 < 0:
+            msg1 = 'Стабильная, прибыльная компания с перспективами роста или компания ' \
+                   'оптимизирующая рентабельность бизнеса без расширения рынков сбыта. ' + \
+                   '\U0001F44D'*3 + ' - Средний кандидат для покупки'
+            return msg1
+
+        elif rank[0] is True and rank[1] is False and rank[2] is True and sma50 > 0:
+            msg1 = 'Развивающаяся компания с краткосрочным падением прибыли и рентабельности. ' + \
+                   '\U0001F44D'*4 + ' - Средний кандидат для покупки. У компании ожидаются прибыли в будущем, ' \
+                                    'и эти ожидания мотивируют инвесторов к покупке акций'
+            return msg1
+        elif rank[0] is True and rank[1] is False and rank[2] is True and sma50 < 0:
+            msg1 = 'Развивающаяся компания с краткосрочным падением прибыли и рентабельности. ' + \
+                   '\U0001F44D'*3 + ' - Средний кандидат для покупки. У компании ожидаются прибыли в будущем, ' \
+                                    'но на данный момент эти ожидания не мотивируют инвесторов к покупке акций.'
+            return msg1
+
+        elif rank[0] is False and rank[1] is True and rank[2] is True and sma50 > 0:
+            msg1 = 'Стабильная компания, без перспектив к росту и развитию в краткосрочной перспективе. ' + \
+                   '\U0001F44D'*4 + ' - Средний кандидат для покупки.'
+            return msg1
+        elif rank[0] is False and rank[1] is True and rank[2] is True and sma50 < 0:
+            msg1 = 'Стабильная компания, без перспектив к росту и развитию в краткосрочной перспективе. ' + \
+                   '\U0001F44D'*3 + ' - Слабый кандидат для покупки.'
+            return msg1
+
+        elif rank[0] is False and rank[1] is False and rank[2] is True and sma50 > 0:
+            msg1 = 'Развивающаяся, но убыточная компания. Возможно компания только захыватывает рынки сбыта. ' + \
+                   '\U0001F44D'*2 + ' - Слабый кандидат для покупки. Компания "на любителя"'
+            return msg1
+        elif rank[0] is False and rank[1] is False and rank[2] is True and sma50 < 0:
+            msg1 = 'Развивающаяся, но убыточная компания. Возможно компания только захыватывает рынки сбыта. ' + \
+                   '\U0001F44D'*1 + ' - Очень слабый кандидат для покупки. Компания "на любителя"'
+            return msg1
+
+        elif rank[0] is False and rank[1] is False and rank[2] is False and sma50 > 0:
+            msg1 = 'Убыточная компания, без перспектив и развития. ' + \
+                   '\U0001F44E' + ' - Не является кандидатом для покупки, может быть кандидатом для спекуляций'
+            return msg1
+        elif rank[0] is False and rank[1] is False and rank[2] is False and sma50 < 0:
+            msg1 = 'Убыточная компания, без перспектив и развития. ' + \
+                   '\U0001F44E' + ' - Не является кандидатом для покупки и спекуляций'
+            return msg1
+
+        elif rank[0] is None and rank[1] is None and rank[2] is None:
+            msg1 = 'Недостаточно данных для скоринга'
+            return msg1
+# 'U+1F44D'
+
+        # elif rank[2] and rank[1] and rank[0]:
+        #     msg1 = 'Стабильная, прибыльная компания c высоким потенциалом роста'
+        #     return msg1
+        # elif rank[2] is False and rank[1] is False and rank[0] is True:
+        #     msg1 = 'Компания убыточная, но увеличивает свою долю на рынке'
+        #     return msg1
+        # elif rank[2] is False and rank[1] is False and rank[0] is False:
+        #     msg1 = 'Убыточная компания. Можно рассматривать только как спекуляцию'
+        #     return msg1
+        # elif rank[2] is False and rank[1] is True and rank[0] is True:
+        #     msg1 = 'Убыточная компания. Можно рассматривать только как спекуляцию'
+        #     return msg1
 
     def stock_news(self):
         try:
