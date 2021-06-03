56d3a5d141888a290a4075618f40cf279cad6d1e
defoer <defoer@gmail.com>
2021-06-03 22:32:40 +0300

    Добавил обновление валюты

--- cron_every_720_min.py
+++ cron_every_720_min.py
@@ -1,7 +1,8 @@
 import argparse
 from project_shared import *
 from quotes.parsers_env import chrome_init
-from quotes.parsers import get_etfdb_flows
+from quotes import sql_queries as sql_q
+from quotes import quote_loader as ql
 from charter.charter import *
 
 
@@ -15,7 +16,10 @@ if __name__ == '__main__':
     debug(f"### Start cron every 720 min scheduler ###")
     img_out_path = PROJECT_HOME_DIR + '/' + IMAGES_OUT_PATH
     debug(img_out_path)
-    get_etfdb_flows(driver=chrome_init(), img_out_path_=img_out_path)
+    # get_etfdb_flows(driver=chrome_init(), img_out_path_=img_out_path)
     # get_flows(driver=chrome_init(), img_out_path_=img_out_path)
+    currency = 'RUB=X'
+    last_usd_price = ql.get_last_currency_price_yq(currency)
+    sql_q.update_last_currency_price(currency, last_usd_price)
     debug("%%%%%%%%%%%%%%%Complete cron every 720\n\n\n")
     debug_deinit()
